f12b6d270168bcce1926ce96374d12cdeb47a0bc
Mark Allison <mark@digital-impacts.org>
2015-07-29 14:54:55 +0100

    Improved Endpoint tests

--- test/Endpoint.spec.js
+++ test/Endpoint.spec.js
@@ -1,5 +1,5 @@
 import assert from 'assert';
-import { request } from 'superagent';
+import nock from 'nock';
 
 import { Endpoint } from '../src/reduxRest';
 
@@ -8,69 +8,91 @@ describe('Endpoint', () => {
         it('should append the object id the the url', () => {
             let endpoint = new Endpoint('');
             let objectURL = endpoint._getObjectURL('obj');
-            assert.equal(objectURL, 'obj');
+            assert.equal(objectURL, '/obj');
         });
     });
     
     describe('list', () => {
-        it('should make a GET request', () => {
-            let endpoint = new Endpoint('');
-            let request = endpoint.list();
-            assert.equal(request.method, 'GET');
+        it('should make a GET request to the endpoint url', (done) => {
+            // Note only giving full url to work with nock
+            // TODO run these tests in browser?
+            let endpoint = new Endpoint('http://example.com/endpoint');
+            // TODO if nock gets support for path regexps we don't need to specify the path here
+            let scope = nock('http://example.com').get('/endpoint').reply(200);
+            let request = endpoint.list().end(() => {
+                scope.done();
+                done();
+            });
         });
         
-        it('should request the url passed to the constructor', () => {
-            let endpoint = new Endpoint('endpoint/');
-            let request = endpoint.list();
-            assert.equal(request.url, 'endpoint/');
-        });
-
         it('should append params object as the query string', () => {
-            let endpoint = new Endpoint('');
-            let request = endpoint.list({key: 'value'});
-            assert.deepEqual(request.qs, {key: 'value'});
+            let endpoint = new Endpoint('http://example.com/endpoint');
+            let scope = nock('http://example.com')
+                .get('/endpoint')
+                .query({key: 'value'})
+                .reply(200);
+            let request = endpoint.list({key: 'value'}).end(() => {
+                scope.done();
+                done();
+            });
+
         });
     });
 
     describe('retrieve', () => {
-        it('should make a GET request', () => {
-            let endpoint = new Endpoint('');
-            let request = endpoint.list();
-            assert.equal(request.method, 'GET');
-        });
-        
-        it('should request the url passed to the constructor with the object id', () => {
-            let endpoint = new Endpoint('endpoint/');
-            let request = endpoint.retrieve('obj');
-            assert.equal(request.url, 'endpoint/obj');
+        it('should make a GET request to the object url', () => {
+            let endpoint = new Endpoint('http://example.com/endpoint');
+            let scope = nock('http://example.com').get('/endpoint/obj').reply(200);
+            let request = endpoint.retrieve('obj').end(() => {
+                scope.done();
+                done();
+            });
         });
     });
 
     describe('create', () => {
-        it('should make a POST request', () => {
-            let endpoint = new Endpoint('');
-            let request = endpoint.create({});
-            assert.equal(request.method, 'POST');
+        it('should make a POST request to the endpoint url', () => {
+            let endpoint = new Endpoint('http://example.com/endpoint');
+            let scope = nock('http://example.com').post('/endpoint').reply(200);
+            let request = endpoint.create().end(() => {
+                scope.done();
+                done();
+            });
         });
         
-        it('should request the url passed to the constructor', () => {
-            let endpoint = new Endpoint('endpoint');
-            let request = endpoint.create({});
-            assert.equal(request.url, 'endpoint');
+        it('should send the object conf in the request body', () => {
+            let endpoint = new Endpoint('http://example.com/endpoint');
+            let scope = nock('http://example.com')
+                .post('/endpoint', {id: 1})
+                .reply(200);
+            let request = endpoint.create({id: 1}).end(() => {
+                scope.done();
+                done();
+            });
         });
+        
     });
 
     describe('update', () => {
-        it('should make a PUT request', () => {
-            let endpoint = new Endpoint('');
-            let request = endpoint.update('obj', {});
-            assert.equal(request.method, 'PUT');
+        it('should make a PUT request to the object url', () => {
+            let endpoint = new Endpoint('http://example.com/endpoint');
+            let scope = nock('http://example.com').put('/endpoint/obj').reply(200);
+            let request = endpoint.update({}, 'obj').end(() => {
+                scope.done();
+                done();
+            });
         });
         
-        it('should request the url passed to the constructor with the object id', () => {
-            let endpoint = new Endpoint('endpoint/');
-            let request = endpoint.update('obj', {});
-            assert.equal(request.url, 'endpoint/obj');
+        it('should send the object conf in the request body', () => {
+            let endpoint = new Endpoint('http://example.com/endpoint');
+            let scope = nock('http://example.com')
+                .put('/endpoint/obj', {id: 1})
+                .reply(200);
+            let request = endpoint.update({id: 1}, 'obj').end(() => {
+                scope.done();
+                done();
+            });
         });
+        
     });
 });
