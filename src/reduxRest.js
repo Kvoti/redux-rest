3597dba75c5370bd99022b6e43e1085c8aa2bfc5
Mark Allison <mark@digital-impacts.org>
2015-08-08 21:02:37 +0100

    Remove jQuery from CSRF token code

--- src/reduxRest.js
+++ src/reduxRest.js
@@ -57,7 +57,7 @@ export class Endpoint {
     if (document.cookie && document.cookie !== '') {
       let cookies = document.cookie.split(';');
       for (let i = 0; i < cookies.length; i++) {
-        let cookie = jQuery.trim(cookies[i]);
+        let cookie = cookies[i].trim();
         // Does this cookie string begin with the name we want?
         if (cookie.substring(0, name.length + 1) === (name + '=')) {
           cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
