dd10e7131a1d287e39e671b7c8aedfe0ce26c79d
bhoomit <bhoomit@mygola.com>
2016-03-03 00:08:42 +0530

    object in place of array

--- src/reduxRest.js
+++ src/reduxRest.js
@@ -220,8 +220,10 @@ export class ItemReducer extends BaseReducer {
       return this._replaceItem(state, 'id', item.id, item);
 
     } else if (action.type === this.actionTypes.list_success) {
-      return [...action.payload];
-
+      if (Array.isArray(action.payload)){
+        return [...action.payload];  
+      }
+      return action.payload;
     }
     return state;
   }
